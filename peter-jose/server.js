cb69023a3fd679c0d887485aaaf0fc401e482a46
Peter Kim <seattlechem@yahoo.com>
2018-02-21 10:07:28 -0800

    app.listen added

--- peter-jose/server.js
+++ peter-jose/server.js
@@ -8,6 +8,10 @@ const express = require('express');
 //instantiate express
 const app = express();
 
+//setting up directory for express
+//it tells the server which directory to serve files from
+app.use(express.static('./public'));
+
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
@@ -15,4 +19,7 @@ app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
-})
+});
+
+// start the app so it listens for changes
+app.listen(PORT,() => console.log(`Listeninbg on port ${PORT}`));

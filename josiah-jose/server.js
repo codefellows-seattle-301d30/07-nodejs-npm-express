9f414eb595f516f18e987f9459ea966d14befe4e
joereyes0123 <rey0123@rey0123s-MacBook-Pro.local>
2018-02-21 15:24:50 -0800

    final

--- josiah-jose/server.js
+++ josiah-jose/server.js
@@ -17,16 +17,13 @@ app.get('/new-article',(request,response) => {
   response.sendFile('/public/new.html',{root: '.'});
 });
 
-app.use(function (req, res){
-  res.status(404).send('ERROR 404');
-});
-
-
-
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 });
 
+app.use(function (req, res){
+  res.status(404).send('ERROR 404');
+});
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
